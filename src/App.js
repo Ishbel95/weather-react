19f37bb4c4367585fb00e5361deb86987b63c227
Ishbel95 <ishbel95@hotmail.com>
2022-07-12 14:53:00 +0100

    set weather details

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import WeatherSearch from "./WeatherSearch";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>hello world</h1>
-        <p>Good morning hello goodbye</p>
-      </header>
+      <header className="App-header"></header>
       <WeatherSearch />
     </div>
   );
--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -8,6 +8,7 @@ export default function WeatherSearch() {
   const [description, setDescription] = useState(null);
   const [humidity, setHumidity] = useState(null);
   const [wind, setWind] = useState(null);
+  const [feel, setFeel] = useState(null);
   const [icon, setIcon] = useState(null);
 
   function showWeather(response) {
@@ -17,6 +18,8 @@ export default function WeatherSearch() {
     setDescription(response.data.weather[0].description);
     setHumidity(response.data.main.humidity);
     setWind(response.data.wind.speed);
+    setFeel(response.data.main.feels_like);
+
     setIcon(iconUrl);
     console.log(response);
   }
@@ -32,14 +35,36 @@ export default function WeatherSearch() {
   }
   let form = (
     <div className="WeatherSearch">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="search"
-          placeholder="Search a City"
-          onChange={updateCity}
-        />
-        <input type="submit" value="Search" />
-      </form>
+      <nav className="Navigation navbar navbar-light">
+        <div className="container-fluid">
+          <form className="d-flex" id="search-form" onSubmit={handleSubmit}>
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Enter a City"
+              id="search-input"
+              title="Search a city"
+              onChange={updateCity}
+            />
+            <button
+              id="search-button"
+              className="btn btn-outline-success"
+              type="submit"
+              title="Search button"
+            >
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+            <button
+              id="geo-location"
+              className="btn btn-outline-success"
+              type="submit"
+              title="Geo-location button"
+            >
+              <i className="fa-solid fa-location-arrow"></i>
+            </button>
+          </form>
+        </div>
+      </nav>
     </div>
   );
 
@@ -47,22 +72,41 @@ export default function WeatherSearch() {
     return (
       <div className="WeatherSearch">
         {form}
+        <h1>{city}</h1>
+        <h2>Last updated: 12:05</h2>
         <ul>
-          <li>Temperature : {Math.round(temperature)}℃</li>
-          <li>Description : {description}</li>
-          <li>Humidity : {Math.round(humidity)}%</li>
-          <li>Wind : {Math.round(wind)}km/h</li>
+          <li>
+            <i className="fa-solid fa-droplet"></i> : {Math.round(humidity)}%
+          </li>
+          <li>
+            <i className="fa-solid fa-wind"></i> : {Math.round(wind)}km/h
+          </li>
+          <li>Feels like:{feel}℃</li>
+          <li>{Math.round(temperature)}℃</li>
           <li>
             <img src={icon} alt={description} />
           </li>
+
+          <li>{description}</li>
         </ul>
       </div>
     );
   } else {
     return (
-      <div>
+      <div className="WeatherSearch">
         {form}
-        <p>Weather for city is loading...</p>
+        <h1>{city}</h1>
+        <ul>
+          <li>Temperature : {Math.round(temperature)}℃</li>
+          <li>Description : {description}</li>
+          <li>
+            <i className="fa-solid fa-droplet"></i> : {Math.round(humidity)}%
+          </li>
+          <li>Wind : {Math.round(wind)}km/h</li>
+          <li>
+            <img src={icon} alt={description} />
+          </li>
+        </ul>
       </div>
     );
   }
